23238ff23b4a860a752d6a38b101df745d906dd4
Fred <fred.webfront@gmail.com>
2020-03-04 13:08:03 -0300

    Authentication service

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+const mongoose = require('mongoose');
+const app = express();
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json( { type: '*/*' } ));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
